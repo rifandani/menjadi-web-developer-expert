783a4325512983582cc891f79a8e00dcbf0ba68e
rifandani <ipandani2505@gmail.com>
2021-04-30 23:19:38 +0700

    :hammer: FIX: background image in hero does not show in app build

--- webpack.common.js
+++ webpack.common.js
@@ -18,6 +18,9 @@ module.exports = {
           },
           {
             loader: 'css-loader',
+            options: {
+              url: false,
+            },
           },
         ],
       },
