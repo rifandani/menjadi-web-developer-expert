e4041863e1db1349e0e514d6d83068f294146d3c
rifandani <ipandani2505@gmail.com>
2021-05-11 15:48:05 +0700

    :hammer: REVIEW: add crossorigin anonymous

--- src/scripts/views/templates/resto-card.js
+++ src/scripts/views/templates/resto-card.js
@@ -1,12 +1,13 @@
+/* eslint-disable indent */
 import CONFIG from '../../global/config';
 
 const restoCard = (resto) => `
     <div tabindex="0" class="card">
       <a href="#/resto/${resto.id}" class="card-a-tag">
         <div class="img-container">
-          <img tabindex="0" class="card-image" alt="${resto.name}" src="${
-  CONFIG.BASE_IMAGE_URL + resto.pictureId
-}"/>
+          <img tabindex="0" class="card-image" crossorigin="anonymous" alt="${
+            resto.name
+          }" src="${CONFIG.BASE_IMAGE_URL + resto.pictureId}"/>
           <span tabindex="0" class="card-rating">
             <i title="ratings" class="fa fa-star"></i>
             <span>${resto.rating}</span>
